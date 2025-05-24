0fb7e3983974a804019377ce49c334cfa13f4afc
TanyaCodeQueen <tanya.she01@gmail.com>
2025-05-24 16:02:09 -0400

    Збереження поточних змін перед pull

--- sample.test.js
+++ sample.test.js
@@ -4,8 +4,8 @@
 import { getMonth, getDate, getDay } from './sample.js';
 
 describe('Перевірка функцій роботи з датами (dateUtils)', () => {
-  const testDate = new Date(2025, 4, 24); // 24 травня 2025 локально
-
+  const testDate = new Date(2025, 4, 24); 
+  
   test('getMonth() має повернути 4 для дати 24.05.2025', () => {
     expect(getMonth(testDate)).toBe(4);
   });
